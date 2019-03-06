9ee67bbf5174dde545750dda34e6e3405fba5929
arun <40155193+just-arun@users.noreply.github.com>
2019-03-06 21:17:28 +0530

    Add files via upload

--- js/pages.js
+++ js/pages.js
@@ -1,44 +1,100 @@
-let staticWebpage = `
-<h3 class="center animated-underline">static webpage</h3>
-        <p>
-            Our static webpage are not like anyother websitesite but contains aditional features like sending contact mail for busness,
-            and paralax effect for awsome looking website all at an resonabel cost, and if the useer prefered then they will get the look and feel of the 
-            android app on the website.
-        </p>
-
-        <h5>Time Span</h5>
-        <p>
-            all our static products are made according to our customers requirement and made within 2-3 days
-        </p>
-
-        <hr>
-        <table>
-            <thead>
-                <th>profuct</th>
-                <th>cost</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><i class="material-icons"> web</i>Simple Static page</td>
-                    <td>Rs.2,500</td>
-                </tr>
-                <tr>
-                    <td><i class="material-icons"> web</i>Portfolio</td>
-                    <td>Rs.3,500</td>
-                </tr>
-                <tr>
-                    <td><i class="material-icons"> add_box</i>Scrolling Effect</td>
-                    <td>Rs.1,000</td>
-                </tr>
-                <tr>
-                    <td><i class="material-icons"> add_box</i>contact form</td>
-                    <td>Rs.500</td>
-                </tr>
-                <tr>
-                    <td><i class="material-icons" >web</i>e-commerce Template</td>
-                    <td>Rs.5,500</td>
-                </tr>
-            </tbody>
-        </table
-`;
-
+let staticWebpage = {
+    title:'Static Page',
+    body:`
+    <h3 class="center animated-underline">static webpage</h3>
+            <p>
+                Our static webpage are not like anyother websitesite but contains aditional features like sending contact mail for busness,
+                and paralax effect for awsome looking website all at an resonabel cost, and if the useer prefered then they will get the look and feel of the 
+                android app on the website.
+            </p>
+    
+            <h5>Time Span</h5>
+            <p>
+                all our static products are made according to our customers requirement and made within 2-3 days
+            </p>
+    
+            <hr>
+            <table>
+                <thead>
+                    <th>profuct</th>
+                    <th>cost</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><i class="material-icons"> web</i>Simple Static page</td>
+                        <td>Rs.2,500</td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons"> web</i>Portfolio</td>
+                        <td>Rs.3,500</td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons"> add_box</i>Scrolling Effect</td>
+                        <td>Rs.1,000</td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons"> add_box</i>contact form</td>
+                        <td>Rs.500</td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons" >web</i>e-commerce Template</td>
+                        <td>Rs.5,500</td>
+                    </tr>
+                </tbody>
+            </table
+    `
+};
+let dynamicWebpage = {
+    title:'Dynamic Page',
+    body:`
+    <h3 class="center animated-underline">Dynamic </h3>
+                <p>
+                    our dynamic pages are diferent from other. our pages are more secure and provide an plesent user experience.
+                    we provide our customers with an beautiful and easy to use dashboard for the user to dynamicaly load the content 
+                    in the webpage. your could use this type of websites to improve your busness growth and make wrapid change to your website without on your own.
+                </p>
+        
+                <h5>Time Span</h5>
+                <p>
+                    all our dynamic products are made according to our customers requirement and made within 4-10 days
+                </p>
+        
+                <hr>
+                <table>
+                    <thead>
+                        <th>profuct</th>
+                        <th>cost</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><i class="material-icons"> web</i>Basic Dynamic page</td>
+                            <td>Rs.8,000</td>
+                        </tr>
+                        <tr>
+                            <td><i class="material-icons"> web</i>Dynamic Portfolio</td>
+                            <td>Rs.8,000</td>
+                        </tr>
+                        <tr>
+                            <td><i class="material-icons" >web</i>e-commerce Template</td>
+                            <td>Rs.5,500</td>
+                        </tr>
+                        <tr>
+                            <td><i class="material-icons"> add_box</i>Scrolling Effect</td>
+                            <td>Rs.1,000</td>
+                        </tr>
+                        <tr>
+                            <td><i class="material-icons"> add_box</i>contact form</td>
+                            <td>Rs.500</td>
+                        </tr>
+                        <tr>
+                            <td><i class="material-icons"> add_box</i>Live chating</td>
+                            <td>Rs.1,500</td>
+                        </tr>
+                        <tr>
+                            <td><i class="material-icons"> add_box</i>Mobile App Look/Feel</td>
+                            <td>Rs.1,000</td>
+                        </tr>
+                    </tbody>
+                </table>
+    `
+}
--- js/riouting.js
+++ js/riouting.js
@@ -1,27 +1,23 @@
-let closeAllSide = ()=>{
-    $('.mainpage-wrapper').css({'display':'block','opacity':'1'});
-    $('#staicWebpage').css('display','none');
-}
-
-
-
-
-let forStaticPage = ()=>{
-    $('.mainpage-wrapper').css({
-        'opacity':'0',
-        'display':'none'
-    });
-    $('#staicWebpage').css({
-        'display':'block',
-        'opacity':1
-    });
-    window.scrollTo(0,0);
-}
-
-$('.home-link').click((e)=>{
-    closeAllSide();
-});
-
-$('.staaticpage-details').click(()=>{
-    forStaticPage();
+var scroolToTop = function () {
+        window.scrollTo(0, 0)
+    },
+    RoutingLogic = function () {
+        "" === window.location.hash || "#" === window.location.hash ? ($("#mainpage-wrapper").css("display", "block"), $("#Title").text("QueueDev"), scroolToTop()) : $("#mainpage-wrapper").css("display", "none"), "#static-webpage" === window.location.hash ? ($("#Title").text(staticWebpage.title), $("#staicWebpage").html(staticWebpage.body), $("#staicWebpage").css({
+            display: "block",
+            opacity: 1
+        }), scroolToTop()) : ($("#staicWebpage").html(""), $("#staicWebpage").css({
+            display: "none",
+            opacity: 0
+        })), "#dynamic-webpage" === window.location.hash ? ($("#Title").text(dynamicWebpage.title), $("#dynamicPage").html(dynamicWebpage.body), $("#dynamicPage").css({
+            display: "block",
+            opacity: 1
+        }), scroolToTop()) : ($("#dynamicPage").html(""), $("#dynamicPage").css({
+            display: "none",
+            opacity: 0
+        }))
+    };
+$(window).on("hashchange", function () {
+    RoutingLogic()
+}), $(document).ready(function () {
+    RoutingLogic()
 });
